36755addadfa2434649159b96e1a00020bfb55e3
Audran Thomas <Audran.Thomas@grenoble-inp.org>
2018-11-27 11:01:11 +0100

    simul

--- RegArchGitEtudiants/RegArchLib/Sources/RegArchCompute.cpp
+++ RegArchGitEtudiants/RegArchLib/Sources/RegArchCompute.cpp
@@ -6,11 +6,16 @@
 	\author Jean-Baptiste DURAND, Ollivier TARAMASCO 
 	\date dec-18-2006 - last change feb-18-2011
 */
-/* namespace RegArchLib {
+namespace RegArchLib {
 
-	// please complete
-	RegArchSimul()
-	{
+    void RegArchSimul(uint Time, cRegArchValue* myData, cRegArchModel model){
+	myData->ReAlloc(Time);
+	for (int i=0; i < Time, i++){
+	    myData->mHt[i] = model.mVar->ComputeVar(i, *myData);
+	    myData->mEpst[i] = ;
+	    myData->mUt[i] = sqrt(mHt[i])*mEpst[i];
+	    myData->mYt[i] = model.mMean->ComputeMean(i, *myData) + mUt[i];
 	}
+    };
 
-}*/ //namespace
+} //namespace
