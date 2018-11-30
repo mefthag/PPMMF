70dff7fc1586b6b5a25134b969d89bf07e4b27f3
besttrainer <ilyasskettioui@gmail.com>
2018-11-30 18:19:25 +0100

    modified the blocs, done some tests, llq always negative !

--- RegArchGitEtudiants/Tests/Source/Tests.cpp
+++ RegArchGitEtudiants/Tests/Source/Tests.cpp
@@ -15,74 +15,54 @@ int main(int argc, char* argv[])
 #endif //WIN32
 {
 	cout.precision(12) ;
-
-	/*********
-	 * ARMA pur
+        /************
+         * Le modele
+         ***********/
+	cRegArchModel myModelArma ;
+        cCondMean myCondMeanArma ;
+	cCondVar myCondVar ;
+        cNormResiduals myNormResid ;
+	myModelArma.SetResid(myNormResid) ;
+        
+        /*********
+	 * Partie ARMA pure
 	 *******/
 	cConst myConst(0.1);
 
 	cAr	myAr(2) ;
-
 	myAr.Set(.8, 0) ;
 	myAr.Set(-.2, 1) ;
-	myAr.Print() ;
 
 	cMa myMa(2) ;
 	myMa.Set(0.8, 0) ;
 	myMa.Set(0.6, 1) ;
         
-
-	cCondMean myCondMeanArma ;
 	myCondMeanArma.SetOneMean(0, myConst) ;
 	myCondMeanArma.SetOneMean(1, myAr) ;
 	myCondMeanArma.SetOneMean(2, myMa) ;
-
-	cConstCondVar myConstVar(1.0) ;
-
-	cCondVar myCondVar ;
-	myCondVar.SetOneVar(0, myConstVar) ;
-
-	cNormResiduals myNormResid ;
-
-	cRegArchModel myModelArma ;
+        
 	myModelArma.SetMean(myCondMeanArma) ;
-	/*myModelArma.SetVar(myCondVar) ;
-	myModelArma.SetResid(myNormResid) ;
-	cout << "Modele : " ;
-	myModelArma.Print() ;
-
-	cRegArchModel myModelArmaCp(myModelArma) ;
-	cout << "Copie du modele : " ;
-	myModelArmaCp.Print() ;*/
-
         
         /*********
-	 *Garch pur
+	 *Partie Garch pure
 	 *******/
-	
-
+        cConstCondVar myConstVar(1.0) ;
+        
 	cArch	myArch(2) ;
-
 	myArch.Set(.9, 0) ;
 	myArch.Set(-.1, 1) ;
-	myArch.Print() ;
 
 	cGarch myGarch(2) ;
 	myGarch.Set(0.14, 0) ;
-	myGarch.Set(0.65, 1) ;
+	myGarch.Set(0.25, 1) ;
         
-
-	
-	
+        myCondVar.SetOneVar(0, myConstVar) ;
 	myCondVar.SetOneVar(1, myArch) ;
 	myCondVar.SetOneVar(2, myGarch) ;
 
-
-
 	myModelArma.SetVar(myCondVar) ;
         
-	myModelArma.SetResid(myNormResid) ;
-	cout << "Modele : " ;
+	cout << "Modele :" ;
 	myModelArma.Print() ;
 
 	cRegArchModel myModelArmaCp(myModelArma) ;
@@ -95,38 +75,29 @@ int main(int argc, char* argv[])
 	cRegArchValue myGivenValue(myNData);
 	for(uint t=0; t < myGivenValue.mYt.GetSize(); t++)
 	{
-		myGivenValue.mYt[t] = t;
+            myGivenValue.mYt[t] = t;
 	}
 
 	cDVector myMeans(myNData);
   
-         
-        
-        
-        
-        
-        ///////////////////////////////////////////////////////////////////////////////////////////////
-        // Moyennes conditionnelles
+
+        // Moyennes et Variances conditionnelles
         myModelArma.GetResid()->Generate(myNData , myGivenValue.mEpst);
 	for(uint t=0; t < myGivenValue.mYt.GetSize(); t++)
 	{    
-		myMeans[t] = myCondMeanArma.ComputeMean(t, myGivenValue);
-		myGivenValue.mUt[t] = myGivenValue.mYt[t] - myMeans[t];
-		//calcul des mHt
-		myGivenValue.mHt[t] = (myGivenValue.mUt[t]*myGivenValue.mUt[t])/(myGivenValue.mEpst[t]*myGivenValue.mEpst[t]);
-                myGivenValue.mMt[t] = myMeans[t];
+            myMeans[t] = myCondMeanArma.ComputeMean(t, myGivenValue);
+            myGivenValue.mUt[t] = myGivenValue.mYt[t] - myMeans[t];
+            //calcul des mHt
+            myGivenValue.mHt[t] = (myGivenValue.mUt[t]*myGivenValue.mUt[t])/(myGivenValue.mEpst[t]*myGivenValue.mEpst[t]);
+            myGivenValue.mMt[t] = myMeans[t];
                 
 	}
         
 	cout << "Moyennes conditionnelles ARMA pur gaussien : " << endl ;
 	myMeans.Print();
 
-
-        
-        
         //Simulation 
-
-	uint myNSample = 100;
+	uint myNSample = 50;
 	cRegArchValue mySimulData = myGivenValue;
 	cDVector mySimulVector(myNSample);
 	RegArchSimul(myNSample,&mySimulData,myModelArma);
@@ -134,15 +105,11 @@ int main(int argc, char* argv[])
 	mySimulVector = mySimulData.mYt;
 	mySimulVector.Print();
 
-        
-        
         // Calcul de vraisemblance
-        double myLoglikelihood = 0.;
-        myLoglikelihood = RegArchLLH(myModelArma, myGivenValue);
-        cout << "Log-vraisemblance : " << myLoglikelihood << endl;
+        double myLogLikelihood = 0.;
+        myLogLikelihood = RegArchLLH(myModelArma, mySimulData);
+        cout << "Log-vraisemblance : " << myLogLikelihood << endl;
         
-	return 0 ;
-
-
+	return 0;
 }
 
